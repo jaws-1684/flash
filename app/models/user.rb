117bb4a9fce92f54ecd0437e71e93b4ba9457c10
jaws <dis.131998@gmail.com>
2026-01-30 11:16:42 +0200

    delete: user_chats model for simplicity and add chat_names method to return the name of the recipient current_user sees

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,21 +9,30 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[github google]
+
   has_many :user_providers, dependent: :destroy
+ 
+
+  validates :username, :slug, uniqueness: :true
   validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 
-  has_many :user_chats
-  has_many :chats, class_name: "Chat", foreign_key: "creator_id", dependent: :destroy
-  validates :username, :slug, uniqueness: :true
+  def chats
+    Chat.where(creator_id: self).or(Chat.where(recipient_id: self))
+  end
 
-  def all_user_chats
-    user_chats = UserChat.where(user_id: self).or(UserChat.where(friend_id: self))
+  def chat_names
+    get_recipient = lambda {|chat| chat.recipient == self ? chat.creator : chat.recipient }
+
+    chats.map do |chat; recipient|
+      recipient = get_recipient.call(chat)
+      {
+        chat_id: chat.id,
+        chat_name: recipient.username,
+        chat_avatar: recipient.avatar
+      }
+    end
   end
   
-  def all_chats
-    Chat.find(all_user_chats.map {|uc| uc.chat_id })
-  end
-
   private
     def create_slug
       self.slug = slug
