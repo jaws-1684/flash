f9b0de5c3f5433294ec58b990d3378fb61a446db
jaws <dis.131998@gmail.com>
2026-01-31 11:40:54 +0200

    feat: add a bar component

---
+++ app/javascript/components/ui/Bar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Logo from '../Icons/Logo'
+
+function Bar({title="Your title"}) {
+  return (
+    <div className='col-span-full row-start-0 row-end-1 flex items-center justify-center p-1 border-b border-solid border-gray-300 dark:border-bgray gap-1 text-gray-200 text-sm font-medium bg-logo dark:bg-fblack select-none z-50 '>
+            <Logo width={16} height={16} className="fill-white dark:fill-gray-400"/>
+            <p>{title}</p>     
+    </div>
+  )
+}
+
+export default Bar
